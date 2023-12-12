054687bec63dc1f4acee02b1f28b65b88b9e920d
zeeshanaf02 <zenaftab02.com>
2023-12-12 15:27:29 +0530

    details

---
+++ git.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	char name[20],usn[20];
+	printf("Enter the name:\n");
+	scanf("%s",name);
+	printf("Enter your usn:\n");
+	scanf("%s",usn);
+	printf("The details are:%s and %s\n",name,usn);
+}
